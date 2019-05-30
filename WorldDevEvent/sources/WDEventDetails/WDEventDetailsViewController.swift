49710de0a4d4284bbca8442eb675c117c0a736a2
bohdans <0000xffff@gmail.com>
2019-05-30 18:52:04 +0300

    World Dev Event - add details view

---
+++ WorldDevEvent/sources/WDEventDetails/WDEventDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WDEventDetailsViewController.swift
+//  WorldDevEvent
+//
+//  Created by Bogdan Sasko on 5/30/19.
+//  Copyright © 2019 vinso. All rights reserved.
+//
+
+import UIKit
+
+class WDEventDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onTouchButtonShowOnMap(_ sender: Any) {
+        print(#function)
+    }
+    
+}
